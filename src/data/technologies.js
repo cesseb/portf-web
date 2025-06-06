e20e0141742fac40809ec40a0180cbd50cb9a2e6
CesarSSH <cesarssh.dev@gmail.com>
2025-06-06 13:49:36 -0600

    Feature: Logic for stack tech

---
+++ src/data/technologies.js
@@ -0,0 +1,38 @@
+export const technologies = [
+  {
+    name: "Typescript",
+    image: "/icons-po/typescript.svg",
+    alt: "Typescript logo",
+  },
+  {
+    name: "JavaScript",
+    image: "/icons-po/javascript.svg",
+    alt: "JavaScript logo",
+  },
+  {
+    name: "Astro",
+    image: "/icons-po/astro.svg",
+    alt: "Astro logo",
+  },
+  {
+    name: "TailwindCSS",
+    image: "/icons-po/tailwind.svg",
+    alt: "TailwindCSS logo",
+  },
+
+  {
+    name: "MySQL",
+    image: "/icons-po/mysql.svg",
+    alt: "MySQL logo",
+  },
+  {
+    name: "PostgreSQL",
+    image: "/icons-po/postgres.svg",
+    alt: "PostgreSQL logo",
+  },
+  {
+    name: "Linux",
+    image: "/icons-po/linux.svg",
+    alt: "Linux logo",
+  },
+];
