a432277224cd3615c09e3bb20063ccf9bdb94c9c
CesarSSH <cesarssh.dev@gmail.com>
2025-06-10 13:37:18 -0600

    Feature: social media profile

---
+++ src/data/SocialMedia.js
@@ -0,0 +1,44 @@
+export const SocialMedia = [
+  {
+    title: "LinkedIn",
+    url: "https://www.linkedin.com/in/your-profile",
+    icon: "/social-media/linkedin.svg",
+    alt: "LinkedIn logo",
+  },
+  {
+    title: "Github",
+    url: "https//github.com/your-profile",
+    icon: "/social-media/github.svg",
+    alt: "GitHub logo",
+  },
+  {
+    title: "Youtube",
+    url: "https://www.youtube.com/channel/your-channel",
+    icon: "/social-media/youtube.svg",
+    alt: "YouTube logo",
+  },
+  {
+    title: "Tiktok",
+    url: "https://www.tiktok.com/@your-profile",
+    icon: "/social-media/tiktok.svg",
+    alt: "TikTok logo",
+  },
+  {
+    title: "Facebook",
+    url: "https://www.facebook.com/your-profile",
+    icon: "/social-media/facebook.svg",
+    alt: "Facebook logo",
+  },
+  {
+    title: "Instagram",
+    url: "https://www.instagram.com/your-profile",
+    icon: "/social-media/instagram.svg",
+    alt: "Instagram logo",
+  },
+    {
+    title: "Threads",
+    url: "https://www.threads.net/@your-profile",
+    icon: "/social-media/threads.svg",
+    alt: "Threads logo",
+  },
+];
